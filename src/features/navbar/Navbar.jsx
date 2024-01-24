2fdf32f6821266c06cc376eaf306e9e08da061e1
HRM0 <103372533+HRM0@users.noreply.github.com>
2024-01-24 11:21:47 -0500

    changed home route on navbar

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   
   return (
     <div className='navbar'>
-      <Link to='/home'>
+      <Link to='/'>
         <img 
         className='navbar__logo'
         src='/images/mass.jpg' 
